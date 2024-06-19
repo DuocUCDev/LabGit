f55a573fcc8a15372df1159bac3e473d2d33d640
Joan Toro Ortiz <joantoro@me.com>
2024-06-18 20:06:59 -0400

    Implementacion de metodo para calcular edad

---
+++ main.py
@@ -0,0 +1,11 @@
+AÑO_ACTUAL = 2024
+def calcular_edad(año_nacimiento):
+    return año_nacimiento - AÑO_ACTUAL
+
+def main():
+    nombre = input("Ingresa tu nombre: ")
+    año_nacimiento = int(input("Ingresa tu año de nacimiento: "))
+    edad = calcular_edad(año_nacimiento)
+    print(f"Hola {nombre}, tienes {edad} años.")
+
+main()
